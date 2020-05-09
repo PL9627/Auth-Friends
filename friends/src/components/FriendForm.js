b205b61b9c4e492db8e56f018f9b6f86c12e7e78
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-05-08 17:40:48 -0700

    finished with submit handler

--- friends/src/components/FriendForm.js
+++ friends/src/components/FriendForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axioswithAuth";
 
 const FriendForm = () => {
   const { newFriendForm, setNewFriendForm } = useState({
@@ -6,6 +7,29 @@ const FriendForm = () => {
     age: "",
     email: "",
   });
+
+  const handleChanges = (e) => {
+    setNewFriendForm({
+      ...newFriendForm,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axiosWithAuth()
+      .post("/friends", newFriendForm)
+      .then((res) => {
+        setNewFriendForm({
+          ...newFriendForm,
+          name: "",
+          age: "",
+          email: "",
+        });
+      })
+      .catch((err) => console.log("FriendForm handleSubmit err", err));
+  };
 };
 
 export default FriendForm;
